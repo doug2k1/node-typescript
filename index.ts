aad7c73db44704f0517fc0f122341aa2b0c8a7da
Douglas Matoso <doug2k1@gmail.com>
2020-05-03 15:39:15 -0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+import express, { json } from "express";
+
+const app = express();
+app.use(json());
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello, TypeScript!" });
+});
+
+app.listen(3000, () => {
+  console.log("🚀 Server started on http://localhost:3000");
+});
